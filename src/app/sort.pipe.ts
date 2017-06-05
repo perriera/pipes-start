9c32c09bfada75cda77ed0bd28540ebde5a5d936
Perry Anderson <perry.anderson@gmail.com>
2017-06-05 18:23:05 -0300

    Added a reverse pipe and a sort pipe.

---
+++ src/app/sort.pipe.ts
@@ -0,0 +1,22 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'sort',
+  pure: false
+})
+export class SortPipe implements PipeTransform {
+
+  transform(value: any[], propName: string): any[] {
+    value.sort((a,b) => {
+      if (a[propName] < b[propName]) {
+        return -1;
+      }
+      if (a[propName] > b[propName]) {
+        return 1;
+      }
+      return 0;
+    })
+    return value;
+  }
+
+}
